550a91e7ddef937a11e1dca6cb606a9bdeea5d86
igorMaposse <igorfernandomaposse@gmail.com>
2021-11-04 12:23:36 +0200

    first commit

---
+++ src/main/java/com/calculator/constants/RabbitMQConstants.java
@@ -0,0 +1,8 @@
+package com.calculator.constants;	
+
+public class RabbitMQConstants {
+	public static final String FILA_SUM="soma";
+	public static final String FILA_MULT="multiplicacao";
+	public static final String FILA_SUBT="subtracao";
+	public static final String FILA_DIV="divisao";
+}
---
+++ src/main/java/com/calculator/dto/RabbitMQDto.java
@@ -0,0 +1,10 @@
+package com.calculator.dto;
+
+import java.io.Serializable;
+
+public class RabbitMQDto implements Serializable {
+	
+	public String firstNumber;
+	public String secondNumber;
+
+} 
